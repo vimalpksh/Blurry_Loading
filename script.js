43d313d2f4e454de975d25985e763e5a1411ba41
vimalpksh <itisvimal@gmail.com>
2023-04-05 17:36:23 +0530

    JS part done, project completed

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 const loadingEl = document.querySelector(".loading");
+const bgEl = document.querySelector(".bg");
+
+let load = 0;
+
+const blurBg = function () {
+  load++;
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadingEl.innerHTML = `${load}%`;
+  loadingEl.style.opacity = scale(load, 0, 100, 1, 0);
+  bgEl.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+
+let int = setInterval(blurBg, 30);
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
